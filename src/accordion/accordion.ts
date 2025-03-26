a44544bf634f9e9f9f0865db3415d484518d0122
sachingupta228 <sachingupta228@yahoo.com>
2025-03-26 12:54:10 +0000

    Document API - updating docs

--- src/accordion/accordion.ts
+++ src/accordion/accordion.ts
@@ -22,7 +22,7 @@ export class SemanticAccordionComponent implements AfterViewInit {
 
   ngAfterViewInit() {
 
-    const inAccordion: HTMLElement|boolean = this.inAccordion(this.accordion.nativeElement, "accordion");
+    let inAccordion: HTMLElement|boolean = this.inAccordion(this.accordion.nativeElement, "accordion");
 
     if (inAccordion) {
       this.accordion.nativeElement.classList.remove("ui");
