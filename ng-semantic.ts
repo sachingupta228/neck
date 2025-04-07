cd9eed83c78e998134ed792d450fa73606d2ae3d
sachingupta228 <sachingupta228@yahoo.com>
2025-04-07 01:01:47 +0000

    Fix UI - improving security

--- ng-semantic.ts
+++ ng-semantic.ts
@@ -56,7 +56,7 @@ export * from "./src/transition/transition";
 export * from "./src/shape/shape";
 export * from "./src/accordion/accordion";
 
-export const SEMANTIC_COMPONENTS: Array<any> = [
+export let SEMANTIC_COMPONENTS: Array<any> = [
  SemanticCardComponent,
  SemanticCardsComponent,
  SemanticContextMenuComponent,
@@ -88,7 +88,7 @@ export const SEMANTIC_COMPONENTS: Array<any> = [
  SemanticRatingComponent
 ];
 
-export const SEMANTIC_DIRECTIVES: Array<any> = [
+export let SEMANTIC_DIRECTIVES: Array<any> = [
  SMTooltipDirective,
  SMVisibilityDirective,
  SMDeviceVisibilityDirective,
