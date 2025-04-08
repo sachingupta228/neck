6da8c98d3720f310d68714ab949455cc12d8db67
sachingupta228 <sachingupta228@yahoo.com>
2025-04-08 23:53:06 +0000

    Revert auth - fixing a bug

--- tools/demo.builder.js
+++ tools/demo.builder.js
@@ -1,9 +1,9 @@
-const SystemBuilder = require('systemjs-builder');
-const builder = new SystemBuilder();
+let SystemBuilder = require('systemjs-builder');
+let builder = new SystemBuilder();
 
 builder.loadConfig(__dirname + '/../systemjs.config.js')
     .then(function(){
-        const outputFile = '../demo/vendor/bundle.min.js';
+        let outputFile = '../demo/vendor/bundle.min.js';
         return builder.buildStatic('app', outputFile, {
             minify: true,
             mangle: true,
